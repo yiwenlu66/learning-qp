17b63f4879fe23e8ac69dbff95e4865ab1d9153c
Yiwen Lu <luyw20@mails.tsinghua.edu.cn>
2023-08-06 03:33:20 +0800

    Study effect of number of QP iterations

--- experiments/tank/run.sh
+++ experiments/tank/run.sh
@@ -2,4 +2,5 @@
 
 # python ../../run.py $1 tank --num-parallel 100000 --horizon 20 --epochs 300 --gamma 0.999 --mini-epochs 1 --exp-name vanilla_rl
 # python ../../run.py $1 tank --num-parallel 100000 --horizon 20 --epochs 300 --mini-epochs 1 --qp-unrolled --exp-name qp_unrolled
-python ../../run.py $1 tank --num-parallel 100000 --horizon 20 --epochs 300 --mini-epochs 1 --qp-unrolled --shared-PH --exp-name qp_unrolled_shared
+# python ../../run.py $1 tank --num-parallel 100000 --horizon 20 --epochs 300 --mini-epochs 1 --qp-unrolled --shared-PH --exp-name qp_unrolled_shared
+python ../../run.py $1 tank --num-parallel 100000 --horizon 20 --epochs 300 --mini-epochs 1 --qp-unrolled --shared-PH --qp-iter 100 --exp-name qp_unrolled_shared_more_iter
